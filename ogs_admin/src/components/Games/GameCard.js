383ab40cd7d160e2ead28549581491192661dd78
Nikola <nikola.wesely.96@gmail.com>
2020-02-26 19:34:32 +0100

    games items

--- ogs_admin/src/components/Games/GameCard.js
+++ ogs_admin/src/components/Games/GameCard.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const GamesCard = ({gameId, imageLink, gameName, price}) => {
 	return (
-		<div >
-			{gameId} {imageLink} {gameName} {price}
+		<div className="w-100 f5 b card shadow-5 ba br4 bw1 pa1 tc ma2 bg-black-50 white">
+			{gameId} | {imageLink} | {gameName} | {price} 
 		</div>
 	);
 }
--- ogs_admin/src/components/Scroll/Scroll.js
+++ ogs_admin/src/components/Scroll/Scroll.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './Scroll.css';
 
-const Scroll = ({children, height, route}) => {
-
-	const buttonSize = (height - 20 - (route === "cart" ? 110 : 87))*0.87 + 'px';
+const Scroll = ({children}) => {
 
 	const divStyle = {
 		overflowY: 'scroll',
-		height: buttonSize,
+		height: "450px",
 	};
 
 	return (
